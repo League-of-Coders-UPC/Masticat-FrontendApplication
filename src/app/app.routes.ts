f25905c0052930b1f1fa36b36c6b6568b2c59ede
ChristianZ <U202011688@upc.edu.pe>
2024-10-08 20:34:27 -0500

    Update Card Profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
+import {ProfileComponent} from "./profile/profile.component";
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
 ];
