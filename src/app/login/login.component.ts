23b4976be625da627cdac0eed3040f4a8a7d25d1
ChristianZ <U202011688@upc.edu.pe>
2024-11-02 17:39:53 -0500

    update:login component validation implementation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -17,25 +18,46 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatFormFieldModule,
     FormsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    CommonModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  @Input('ngModel') email: string = '';
-  @Input('ngModel') password: string = '';
+  @Input() email: string = '';
+  @Input() password: string = '';
+  isErrorForm: { email?: string; password?: string } = {};
 
   constructor(private authService: AuthService, private router: Router) { }
 
+  validateForm(): boolean {
+    this.isErrorForm = {};
+
+    if (!this.email) {
+      this.isErrorForm.email = 'Email is required.';
+    } else if (!this.email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+      this.isErrorForm.email = 'Ingrese un email válido.';
+    }
+
+    if (!this.password) {
+      this.isErrorForm.password = 'Password is required.';
+    }
+
+    return Object.keys(this.isErrorForm).length === 0;
+  }
+
   onSubmit(): void {
-    this.authService.login(this.email, this.password).subscribe(
-      () => {
-        this.router.navigate(['/dashboard']);
-      },
-      error => {
-        console.error('Login failed', error);
-      }
-    );
+    if (this.validateForm()) {
+      this.authService.login(this.email, this.password).subscribe(
+        () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error => {
+          console.error('Login failed', error);
+          this.isErrorForm.password = 'Login failed. Verify your email and password.';
+        }
+      );
+    }
   }
 }
